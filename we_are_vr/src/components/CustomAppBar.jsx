8f70891cc1f0c8607c39217eb176355d56ef4b16
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-03-02 20:01:16 -0700

    added the conent section

---
+++ we_are_vr/src/components/CustomAppBar.jsx
@@ -0,0 +1,30 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import SearchFunction from './SearchFunction';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+
+            <Typography >WEAREVR</Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <SearchFunction />
+          </Typography>
+
+          <Box sx={{ display: 'flex', mr: '1em' }}>
+          <HomeIcon fontSize='large'/>
+
+          </Box>
+          <PersonIcon  fontSize='large'/>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- we_are_vr/src/pages/Home.jsx
+++ we_are_vr/src/pages/Home.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import CustomAppBar from '../components/CustomAppBar';
+import WelcomeMessage from "../components/WelcomeMessage";
+import { Box } from '@mui/material';
+import TrendingComponent from '../components/TrendingComponent';
+
 
 function Home({ setPage }) {
   return (
@@ -6,6 +12,21 @@ function Home({ setPage }) {
       <h1>Home Page</h1>
       <button onClick={() => setPage('forum')}>Go to Forum</button>
       <button onClick={() => setPage('profile')}>Go to Profile</button>
+      <Typography>Hello World</Typography>
+      <CustomAppBar />
+      <WelcomeMessage isLoggedIn={false} name={"Bobby"}/>
+      <Box 
+            // height={200}
+            my={4}
+            // display="flex"
+            alignItems="center"
+            justifyItems={'center'}
+            gap={4}
+            p={2}
+            sx={{ border: '2px solid grey' }}
+      >
+        <TrendingComponent />
+      </Box>
     </div>
   );
 }
