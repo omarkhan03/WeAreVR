9047760708c74b568c2f2f616a91fba6ffe6f6c0
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-03-06 15:25:25 -0700

    created the visit profile page

---
+++ we_are_vr/src/pages/VisitProfile.jsx
@@ -0,0 +1,33 @@
+import "./visitProfile.css";
+import CustomAppBar from "../components/CustomAppBar";
+
+function VisitProfile({ setPage }) {
+  return (
+    <>
+
+      <CustomAppBar />
+      <div className="VisitProfile-cover-photo"></div>
+      <div className="profile-section">
+        <div className="profile-picture">
+          <img src="../../images/gorilla.jpeg" alt="Profile" />
+        </div>
+      </div>
+      <div className="profile-name">
+        {" "}
+        {/* Add this div for the name */}
+        <h2 style={{ color: "white" }}>{"Gorrila Boy"}</h2>{" "}
+        {/* Display the name */}
+      </div>
+      <div className="description-section">
+        <textarea
+          className="description-edit"
+          value={"Welcome to my profile! I interested in VR and AR technologies. I love playing VR games especially gorilla tag. "}
+          // I want to make this view only
+          readOnly
+        />
+      </div>
+    </>
+  );
+}
+
+export default VisitProfile;
