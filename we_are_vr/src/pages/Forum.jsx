5456d7098ffd07f4377b790dc99219652a0c8291
omarkhan03 <omarahmadkhan2003@gmail.com>
2024-04-04 14:05:43 -0600

    messaging and likes

--- we_are_vr/src/pages/Forum.jsx
+++ we_are_vr/src/pages/Forum.jsx
@@ -1,124 +1,128 @@
-import React from 'react';
-import "./Forum.css"
+import React, { useState } from 'react';
+import "./Forum.css";
 import { Box } from "@mui/material";
 import SideBar from "../components/SideBar";
-
-import ButtonAppBar from '../components/CustomAppBar';
-import { color } from '@mui/system';
-
-
 import CustomAppBar from "../components/CustomAppBar";
 
-
 const ForumPage = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [messages, setMessages] = useState([
+    {
+      id: 1,
+      user: "GorillaBoy",
+      date: "Mar 6 3:02am",
+      text: "Hey do you wanna see some awesome clips I recorded yesterday?",
+      imgSrc: "../../images/gorilla2.jpeg",
+    },
+    {
+      id: 2,
+      user: "Monkey3",
+      date: "Mar 6 3:02am",
+      text: "Sure lets see them.",
+      imgSrc: "../../images/gorilla3.webp",
+    },
+    {
+      id: 3,
+      user: "GorillaBoy",
+      date: "Mar 6 3:02am",
+      text: "Aight here it is! Enjoy",
+      imgSrc: "../../images/gorilla2.jpeg",
+    },
+    {
+      id: 4,
+      user: "GorillaBoy",
+      date: "Mar 6 3:15am",
+      text: `<iframe src="https://www.youtube.com/embed/4aLNmN1DVts" frameborder="0" allowfullscreen></iframe>`,
+      imgSrc: "../../images/gorilla2.jpeg",
+      likes: 3, // like count state for video post
+    },
+  ]);
+
+  const handleLike = (id) => {
+    setMessages(messages.map(message => {
+      if (message.id === id) {
+        return { ...message, likes: message.likes + 1 };
+      }
+      return message;
+    }));
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputValue.trim()) return;
+
+    const newMessage = {
+      id: messages.length + 1,
+      user: "Aryan",
+      date: new Date().toLocaleString(),
+      text: inputValue,
+      imgSrc: "../../images/profile.png",
+    };
+
+    setMessages([...messages, newMessage]);
+    setInputValue('');
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      handleSubmit(e);
+    }
+  };
 
   return (
     <div>
-      <Box
-        sx={{
-          display: "grid",
-          gridTemplateColumns: "250px 1fr",
-          background: "inherit",
-        }}
-      >
-        
+      <Box sx={{ display: "grid", gridTemplateColumns: "250px 1fr", background: "inherit" }}>
         <SideBar />
         <div className="forum-page">
-
-        <ButtonAppBar></ButtonAppBar>
-
-        <header className="forum-header">
-          <p style={{color:"black"}}>.</p>
-
-          <div>
-            <img src="../../images/gorilla.jpeg" alt="" />
-            <h2>Gorilla Tag</h2>
-          </div>
-
-          <div className="header-icons">
-            <button className="about-button">About</button>
-            <button className="join-button">Join Forum</button>
-
-          </div>
-        </header>
-
-
-        <main className="forum-main">
-        <div className="messages-container">
-          {/* Sample messages */}
-
-
-          <div className="message">
-            <img src="../../images/gorilla2.jpeg" alt="" />
-            <div>
-              <div>
-                <div className="message-user">GorillaBoy</div>
-                <div className="date">Mar 6 3:02am</div>
-              </div>
-              <div className="message-text">Hey do you wanna see some awesome clips I recorded yesterday?</div>
-            </div>
-          </div>
-
-          <div className="message">
-            <img src="../../images/gorilla3.webp" alt="" />
-            <div>
-              <div>
-                <div className="message-user">Monkey3</div>
-                <div className="date">Mar 6 3:02am</div>
-              </div>
-              <div className="message-text">Sure lets see them.</div>
-            </div>
-          </div>
-
-
-          <div className="message">
-            <img src="../../images/gorilla2.jpeg" alt="" />
-            <div>
-              <div>
-                <div className="message-user">GorillaBoy</div>
-                <div className="date">Mar 6 3:02am</div>
-              </div>
-              <div className="message-text">Aight here it is! Enjoy</div>
-            </div>
-          </div>
-
-          <div className="message">
-            <img src="../../images/gorilla2.jpeg" alt="" />
-            <div>
-              <div>
-                <div className="message-user">GorillaBoy</div>
-                <div className="date">Mar 6 3:02am</div>
-              </div>
-              <div className='video-post'>
-                <iframe src="https://www.youtube.com/embed/4aLNmN1DVts" frameborder="0"></iframe>
-                <div>
-                <button>Like (3)</button>
-                <button>Reply (0)</button>
-                <button>Share</button>
-                  
+          <CustomAppBar />
+          <header className="forum-header">
+            {/* Header content as before */}
+          </header>
+          <main className="forum-main">
+            <div className="messages-container">
+              {messages.map((message) => (
+                <div key={message.id} className="message">
+                  <img src={message.imgSrc} alt="" />
+                  <div>
+                    <div>
+                      <div className="message-user">{message.user}</div>
+                      <div className="date">{message.date}</div>
+                    </div>
+                    <div className="message-text" dangerouslySetInnerHTML={{ __html: message.text }}></div>
+                    {message.likes !== undefined && (
+                      <div className="video-post-actions">
+                        <button style={{ backgroundColor: "#0056b3", color: "white" }} onClick={() => handleLike(message.id)}>
+                          Like ({message.likes})
+                        </button>
+                        <button style={{ backgroundColor: "#0056b3", color: "white" }}>Reply (0)</button>
+                        <button style={{ backgroundColor: "#0056b3", color: "white" }}>Share</button>
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-
+              ))}
             </div>
-          </div>
-
-
-
-          {/* Repeat for each message */}
+            <div className="message-input-container">
+              <button className="send-message-button" style={{marginRight:"1rem"}}>Upload media</button>
+              <input
+                type="text"
+                className="message-input"
+                placeholder="Type a message..."
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyPress={handleKeyPress}
+              />
+              <button className="send-message-button" onClick={handleSubmit}>Send</button>
+            </div>
+          </main>
         </div>
-        <div className="message-input-container">
-          <button className="send-message-button" style={{marginRight:"1rem"}}>Upload media</button>
-          <input type="text" className="message-input" placeholder="Type a message..." />
-          <button className="send-message-button">Send</button>
-        </div>
-      </main>
-
-</div>
       </Box>
     </div>
   );
-
-
 };
 
 export default ForumPage;
