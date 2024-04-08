545b8c41ff17518de13a45bac6ef71800390463f
ARYAN TANEJA <taneja.aryan2000@gmail.com>
2024-04-08 15:33:01 -0600

    Second profile

---
+++ we_are_vr/src/pages/SecondProfile.jsx
@@ -0,0 +1,32 @@
+import "./SecondProfile.css";
+import CustomAppBar from "../components/CustomAppBar";
+
+function SecondProfile({ setPage }) {
+  return (
+    <>
+      <CustomAppBar />
+      <div className="VisitProfile-cover-photo"></div>
+      <div className="profile-section">
+        <div className="profile-picture">
+          <img src="../../images/steve.png" alt="Profile" />
+        </div>
+      </div>
+      <div className="profile-name">
+        {" "}
+        {/* Add this div for the name */}
+        <h2 style={{ color: "white" }}>{"Gorrila Kid"}</h2>{" "}
+        {/* Display the name */}
+      </div>
+      <div className="description-section">
+        <textarea
+          className="description-edit"
+          value={"Welcome to my profile! Please follow me on youtube"}
+          // I want to make this view only
+          readOnly
+        />
+      </div>
+    </>
+  );
+}
+
+export default SecondProfile;
